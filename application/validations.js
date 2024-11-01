a324a14ff4a78e401818e60c828ddb0d1981f977
Daniel Tavella <danieltavella0@gmail.com>
2024-10-31 22:05:07 -0300

    Atualizações
    Feito atualização nas src dos scripits

--- application/validations.js
+++ application/validations.js
@@ -1,17 +1,39 @@
 // função utilizada para validar Email
+
+
 function validateEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
 }
 
 
-function menu() {
-    window.location.href = "ativaUser.html"
-    }
+// function menu() {
+//     firebase.auth().signInWithEmailAndPassword( emailForm.email().value, passwordForm.password().value).then(response =>{
+//         window.location.href ="../tela-usuario/ativaUser.html"
+//     }).catch(error => {
+//         console.log("error", error)
+//     });
+//     }
     
-function cadastro(){
-    window.location.href = "ativaCadas.html"
-}
+// function cadastro(){
+//     window.location.href = "../tela-cadas/ativaCadas.html"
+// }
 
-function login() {
-    window.location.href = "ativaLogin.html"
-}
+// function login() {
+//     window.location.href = "../tela-login/ativaLog.html"
+// }
+
+// const emailForm = { // - objeto para o campo de email
+//     email: () => document.getElementById("email"), // --- puxa a informação do ID email
+//     errorEmpty: () => document.getElementById("error-email-empty"), // --puxa a informação do ID do erro do campo vazio
+//     errorInvalid: () => document.getElementById("error-email-invalid"), // --puxa a informação do ID do erro do campo invalido
+//     buttonRecpass: () => document.getElementById("button-Recpass"), // pux informação do bt de recuperar senha
+//     buttonLog: () => document.getElementById("button-log") // puxa informação do bt de login
+
+// }
+
+// //  facilitações no codigo para puxar status dos ID referente ao campo de senha
+// const passwordForm = {
+//     password: () => document.getElementById("password"),
+//     errorEmptyPass: () => document.getElementById("error-senha-empty")
+
+// }
